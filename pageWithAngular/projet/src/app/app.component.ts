0dd8b7efd0d6580a18eda975f8a5f46a33889c1c
pachau <pascal.chayer@polymtl.ca>
2018-01-20 23:58:11 -0500

    PC: ptit modif pour le texte

--- pageWithAngular/projet/src/app/app.component.ts
+++ pageWithAngular/projet/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DROOM TOOM';
+  title = 'Your City As Art';
   MapLocation = 'Welcome to down-town Montreal ... your new museum!';
   lat: number = 45.5088400;
   lng: number =  -73.5878100;
