dc0a3b9fa3cafb26ddd14bcb439c666e95f7a668
Audrey Germain <audrey.germain@polymtl.ca>
2018-01-21 08:36:30 -0500

    AG: modification du front end

--- pageWithAngular/projet/src/app/app.component.ts
+++ pageWithAngular/projet/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Your City As Art';
-  MapLocation = 'Welcome to down-town Montreal ... your new museum!';
+  title = 'Votre ville, un art';
+  MapLocation = 'Bienvenue à Montréal ... votre nouveau musée!';
   lat: number = 45.509881;
   lng: number =  -73.563838;
   zoom: number = 12;
